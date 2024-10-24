76c4751e0460380729afa20f58558de10bece484
Murilo B. Scorsato <scorsatomurilo@gmail.com>
2024-10-24 10:00:00 -0300

    urls sistema_django configurada para receber arquivos de fotos

--- index/models.py
+++ index/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 
+class LadoDireito(models.Model):
+    foto = models.ImageField(upload_to='img_Direito')
+    zap = models.CharField(max_length=30)
+    instagram = models.TextField(max_length=50)
+    youtube = models.TextField(max_length=50)
+    linkedin = models.TextField(max_length=50)
+    email = models.EmailField(max_length=50)
+    texto = models.TextField()
+
+    def __str__(self):
+        return 'Editar Lado Direito'
