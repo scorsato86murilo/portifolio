0356af8f0c950620280aa0d83c17babf97fdd48c
Murilo B. Scorsato <scorsatomurilo@gmail.com>
2024-11-07 15:18:52 -0300

    gestao_salario, salvado salario no BD, melhorando

--- gestao_salario/models.py
+++ gestao_salario/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Salario(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)  # Associa ao usuário logado
-    salario = models.FloatField(max_length=25)
+    salario = models.FloatField()  # max_length não é necessário para FloatField
 
     def __str__(self):
-        return f"{self.usuario.username} - {self.salario}"
+        return str(self.salario)
