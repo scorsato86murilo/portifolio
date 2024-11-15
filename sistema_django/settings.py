08409e022a98cd8f8e4cae73bf66be35de48b866
Murilo B. Scorsato <scorsatomurilo@gmail.com>
2024-11-15 06:45:15 -0300

    settings.py

--- sistema_django/settings.py
+++ sistema_django/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-!(^_7fidc!&hoteza=fgnvi8p@$%o*l^n##md9-gz9^n^i$#9y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
