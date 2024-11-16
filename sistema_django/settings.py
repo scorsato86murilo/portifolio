8c2ea1c774d60129728921d62fc6e6d00990dd3d
Murilo B. Scorsato <scorsatomurilo@gmail.com>
2024-11-16 18:00:27 -0300

    settings.py STATIC_ROOT = agoravai6

--- sistema_django/settings.py
+++ sistema_django/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!(^_7fidc!&hoteza=fgnvi8p@$%o*l^n##md9-gz9^n^i$#9y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
